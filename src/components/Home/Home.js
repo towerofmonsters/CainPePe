d13eba885ffcf9ace8dc95e167ed9caae132606f
unknown <renzivan102013@gmail.com>
2023-07-14 17:56:06 +0800

    ca

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -98,7 +98,7 @@ const Home = () => {
                                 <div className="text-black my-10">
                                     <Label>
                                         <span className=" text-black text-base">CA</span>
-                                        <Label.Detail className=" text-base text-dark-green" >0x087987Cfd4E3DcE9Ebf8708AB6466593DCaD73F2</Label.Detail>
+                                        <Label.Detail className=" text-base text-dark-green" ></Label.Detail>
 
                                     </Label>
                                 </div>
